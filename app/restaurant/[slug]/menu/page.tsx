f1def5dd5617f2d168c7f1f3e8ef69e14922c74c
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-10-03 15:55:10 -0400

    Divide the pages into components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,44 +1,26 @@
+import Header from '../../components/Header';
 import RestaurantNav from '../../components/RestaurantNav';
+import MenuCard from './components/MenuCard';
 
 const RestaurantDetailMenuPage = () => {
   return (
     <>
-      {/* NAVBAR */} {/* HEADER */}
-      <div className='h-96 overflow-hidden'>
-        <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
-          <h1 className='text-7xl text-white captitalize text-shadow text-center'>
-            Milestones Grill (Toronto)
-          </h1>
-        </div>
-      </div>
-      {/* HEADER */} {/* DESCRIPTION PORTION */}
+      <Header />
       <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
         <div className='bg-white w-[100%] rounded p-3 shadow'>
-          {/* RESAURANT NAVBAR */}
           <RestaurantNav slug='/ass' />
-          {/* RESAURANT NAVBAR */} {/* MENU */}
           <main className='bg-white mt-5'>
             <div>
               <div className='mt-4 pb-1 mb-1'>
                 <h1 className='font-bold text-4xl'>Menu</h1>
               </div>
               <div className='flex flex-wrap justify-between'>
-                {/* MENU CARD */}
-                <div className=' border rounded p-3 w-[49%] mb-3'>
-                  <h3 className='font-bold text-lg'>Surf and Turf</h3>
-                  <p className='font-light mt-1 text-sm'>
-                    A well done steak with lobster and rice
-                  </p>
-                  <p className='mt-7'>$80.00</p>
-                </div>
-                {/* MENU CARD */}
+                <MenuCard />
               </div>
             </div>
           </main>
-          {/* MENU */}
         </div>
       </div>
-      {/* DESCRIPTION PORTION */}
     </>
   );
 };
---
+++ app/restaurant/components/Reviews.tsx
@@ -0,0 +1,16 @@
+import ReviewCard from './ReviewCard';
+
+const Reviews = () => {
+  return (
+    <div>
+      <h1 className='font-bold text-3xl mt-10 mb-7 borber-b pb-5'>
+        What 100 people are saying
+      </h1>
+      <div>
+        <ReviewCard />
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
