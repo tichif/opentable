baad51810ff502bdfeed00fb23b78e068fc57439
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-10-03 17:10:50 -0400

    Create nested layout

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,27 +1,22 @@
-import Header from '../../components/Header';
 import RestaurantNav from '../../components/RestaurantNav';
 import MenuCard from './components/MenuCard';
+import RestaurantLayout from '../../layout';
 
 const RestaurantDetailMenuPage = () => {
   return (
-    <>
-      <Header />
-      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-        <div className='bg-white w-[100%] rounded p-3 shadow'>
-          <RestaurantNav slug='/ass' />
-          <main className='bg-white mt-5'>
-            <div>
-              <div className='mt-4 pb-1 mb-1'>
-                <h1 className='font-bold text-4xl'>Menu</h1>
-              </div>
-              <div className='flex flex-wrap justify-between'>
-                <MenuCard />
-              </div>
-            </div>
-          </main>
+    <div className='bg-white w-[100%] rounded p-3 shadow'>
+      <RestaurantNav slug='/ass' />
+      <main className='bg-white mt-5'>
+        <div>
+          <div className='mt-4 pb-1 mb-1'>
+            <h1 className='font-bold text-4xl'>Menu</h1>
+          </div>
+          <div className='flex flex-wrap justify-between'>
+            <MenuCard />
+          </div>
         </div>
-      </div>
-    </>
+      </main>
+    </div>
   );
 };
 
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -6,23 +6,21 @@ import ReservationCard from '../components/ReservationCard';
 import RestaurantNav from '../components/RestaurantNav';
 import Reviews from '../components/Reviews';
 import Title from '../components/Title';
+import RestaurantLayout from '../layout';
 
 const RestaurantDetailPage = () => {
   return (
     <>
-      <Header />
-      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-        <div className='bg-white w-[70%] rounded p-3 shadow'>
-          <RestaurantNav slug='ass' />
-          <Title />
-          <Rating />
-          <Description />
-          <Images />
-          <Reviews />
-        </div>
-        <div className='w-[27%] relative text-reg'>
-          <ReservationCard />
-        </div>
+      <div className='bg-white w-[70%] rounded p-3 shadow'>
+        <RestaurantNav slug='ass' />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className='w-[27%] relative text-reg'>
+        <ReservationCard />
       </div>
     </>
   );
