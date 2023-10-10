b0a80df63e422a46a672eb9b00dfdb332d9495c1
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-10-10 15:58:11 -0400

    Install and configure Prisma

--- prisma/migrations/20231010195140_first_migration/migration.sql
+++ prisma/migrations/20231010195140_first_migration/migration.sql
@@ -24,7 +24,7 @@ CREATE TABLE "Restaurant" (
 CREATE TABLE "Item" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
+    "price" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "restaurant_id" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
